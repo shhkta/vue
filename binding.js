fcee99a419f9c798e9bbac4213cfd906e5106af6
Your Name <you@example.com>
2018-06-30 21:47:07 +0900

    commit

---
+++ binding.js
@@ -0,0 +1,13 @@
+var vue = new Vue({
+    el: '#app',
+    data: {
+        text: 'vue.js',
+        array: ['apple', 'orange', 'banana'],
+        object: {name: 'apple', price: 300},
+        num: 1234,
+        boolene: true,
+        textcolor: 'red',
+        bgcolor: 'black',
+        style:{color: 'yellow', backgroundColor: 'green'}
+    }
+})
